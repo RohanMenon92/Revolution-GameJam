96a0ec7420bc89a9d1b4b0c47cd4bafe40fd066e
FERAS HATHAF <1905157@uad.ac.uk>
2019-10-26 19:50:03 +0100

    UI Implementation

---
+++ Assets/Scripts/HighScoreTracker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HighScoreTracker : MonoBehaviour
+{
+    public int highScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextMeshProUGUI scoreText = GetComponent<TextMeshProUGUI>();
+        scoreText.text = "High Score = " + highScore;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/staminaBarController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class staminaBarController : MonoBehaviour
+{
+    float barLength = 300;
+    public float tiredConstant = 100f;
+
+    RectTransform currentStamina;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentStamina = GetComponent<RectTransform>();
+        currentStamina.sizeDelta = new Vector2(barLength, 20);
+
+        //GetComponent<RectTransform>().sizeDelta = new Vector2(barLength, 20);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (barLength > 0)
+        {
+            barLength -= Time.deltaTime * tiredConstant;
+            currentStamina.sizeDelta = new Vector2(barLength , 20);
+        }
+    }
+}
